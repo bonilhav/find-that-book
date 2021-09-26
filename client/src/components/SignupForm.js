bfb8a1d395b3792cf6fc7003bcf896891409c341
Vanessa Bonilha <81935432+bonilhav@users.noreply.github.com>
2021-09-26 11:17:31 -0600

    code for converting to graphql on client side

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+/* import { createUser } from '../utils/API'; */
 import Auth from '../utils/auth';
+
+//added code
+import { useMutation } from '@apollo/client'
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +15,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  //added code
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +35,18 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: {...userFormData}
+      });
+      /* const response = await createUser(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
       const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      console.log(user); */
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+/* import { getMe, deleteBook } from '../utils/API'; */
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+//added code
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutuations'
+
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK)
+  const userData = data?.me || {};
+
+/*  const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
@@ -34,7 +43,7 @@ const SavedBooks = () => {
     };
 
     getUserData();
-  }, [userDataLength]);
+  }, [userDataLength]); */
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -45,14 +54,17 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      const { data } = await removeBook({
+        variables: { bookId }
+      });
+      /* const response = await deleteBook(bookId, token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
       const updatedUser = await response.json();
-      setUserData(updatedUser);
+      setUserData(updatedUser); */
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -61,7 +73,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
